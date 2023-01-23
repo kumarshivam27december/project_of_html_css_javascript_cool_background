c9d81e79399ee160ba07d69333cc8096a429504a
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-01-23 23:40:13 +0530

    version 2.0

--- i.js
+++ i.js
@@ -1,8 +1,26 @@
-const collection= document.getElementsByClassName("stupid");
 
+
+function clicke3(){
+     
+document.body.style.backgroundImage = "url('https://www.bing.com/th/id/OGC.8e739eb5c8624febbab4600e5050570c?pid=1.7&rurl=https%3a%2f%2fi.pinimg.com%2foriginals%2fa3%2f36%2f29%2fa33629e2815e1343a77b49f9db8bccd3.gif&ehk=HNS3sb3t4RUEacb3W2%2fdwaUqpkuzaBFfydyP6ZX5Vg8%3d')"
+}
+function clicker(){
+    document.body.style.backgroundImage = "url('https://www.bing.com/th/id/OGC.ec6b4235dd8a4a80149f7a01d0e3356a?pid=1.7&rurl=https%3a%2f%2fmedia.giphy.com%2fmedia%2fNA5S7F2dikADu%2fgiphy.gif&ehk=7AR71ZxVDa%2fjsRMZP847onsdUEGVaxca%2bSUlnj4kmQ4%3d')"
+
+}
+function clicke1(){
+    document.body.style.backgroundImage = "url('https://www.bing.com/th/id/OGC.585f9d85cf08b4d379a11a2741b1ab6e?pid=1.7&rurl=http%3a%2f%2fi1239.photobucket.com%2falbums%2fff513%2fRamapoetra24%2fRAMyellowmatrix.gif&ehk=GhkK9Qy8w94IuztOufeOaXSTGv40Gdn3AfNNBe4gmbo%3d')"
+
+}
+function clicke2(){
+    document.body.style.backgroundImage = "url('https://th.bing.com/th/id/OIP.ermtLiWirwTmHb4RQLpufAAAAA?w=139&h=150&c=7&r=0&o=5&dpr=1.5&pid=1.7')"
+
+}
 function clicked(){
-    // Document.innerHTML.style.background-image:url();
-  const collection= document.getElementsByClassName("stupid");
-  collection.style.backgroundImage = "url('https://www.bing.com/th/id/OGC.8e739eb5c8624febbab4600e5050570c?pid=1.7&rurl=https%3a%2f%2fi.pinimg.com%2foriginals%2fa3%2f36%2f29%2fa33629e2815e1343a77b49f9db8bccd3.gif&ehk=HNS3sb3t4RUEacb3W2%2fdwaUqpkuzaBFfydyP6ZX5Vg8%3d')";  
+    document.body.style.backgroundImage = "url('https://th.bing.com/th/id/OIP.UOAv1g5HUPOjoDwJ_oEF-gHaHa?w=162&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7')"
+
+}
+function clicke4(){
+    document.body.style.backgroundImage = "url('https://www.bing.com/th/id/OGC.081188aae6f349162ec2245b0eeff8fc?pid=1.7&rurl=http%3a%2f%2fi480.photobucket.com%2falbums%2frr169%2ffranstudio%2fLake%2fmatrix%2fBlue-Matrix-Code-Lg.gif&ehk=t3hTbeqZP3ytzB50PK2D9d1D1lJwy76WqHJT%2fvqaEdw%3d')"
 
 }
