7f85ed36fb82d6f7e28de199e26abdf25b6d570f
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-01-23 17:02:25 +0530

    version 1.0

---
+++ i.js
@@ -0,0 +1,8 @@
+const collection= document.getElementsByClassName("stupid");
+
+function clicked(){
+    // Document.innerHTML.style.background-image:url();
+  const collection= document.getElementsByClassName("stupid");
+  collection.style.backgroundImage = "url('https://www.bing.com/th/id/OGC.8e739eb5c8624febbab4600e5050570c?pid=1.7&rurl=https%3a%2f%2fi.pinimg.com%2foriginals%2fa3%2f36%2f29%2fa33629e2815e1343a77b49f9db8bccd3.gif&ehk=HNS3sb3t4RUEacb3W2%2fdwaUqpkuzaBFfydyP6ZX5Vg8%3d')";  
+
+}
